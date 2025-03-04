40d9acd7fb35694eec8826269b5d39937643d3eb
jumainfomagnus <163571282+jumainfomagnus@users.noreply.github.com>
2025-03-03 23:20:17 -0500

    fixed the tf file/removed extra comment

--- terraform/public_s3_bucket.tf
+++ terraform/public_s3_bucket.tf
@@ -5,7 +5,7 @@ provider "aws" {
 resource "aws_s3_bucket" "public_bucket" {
   bucket = "my-public-bucket"
 
-  # ❌ Vulnerability: Public read access exposes sensitive data to the internet.
+  
   acl    = "public-read"
 
   tags = {
